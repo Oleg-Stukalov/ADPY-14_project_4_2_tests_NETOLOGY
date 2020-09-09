9b22e6b5345dcaf9c37f80a88fe1c57457528ad9
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-09-09 16:03:36 +0300

    test_delete_doc modified

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from app import get_doc_owner_name, show_all_docs_info, add_new_doc, delete_doc, remove_doc_from_shelf
+from app import get_doc_owner_name, show_all_docs_info, add_new_doc, delete_doc, remove_doc_from_shelf, append_doc_to_shelf, update_date, documents
 
 NL = '\n'
 
@@ -7,7 +7,7 @@ class TestFunctions(unittest.TestCase):
 
     #setUp method is overridden from the parent class TestCase
     def setUp(self):
-        pass
+        update_date()
         #self.user_doc_number() = user_doc_number()
         #self.functions = Functions_5_1()
 
@@ -30,10 +30,32 @@ class TestFunctions(unittest.TestCase):
         }
         self.assertEqual(add_new_doc(), new_doc_shelf_number)
 
-    def test_delete_doc(self):  # D
-        user_doc_number = '123'
-        delete_doc()
-        self.assertEqual(remove_doc_from_shelf(user_doc_number), user_doc_number)
+    def test_delete_doc(self):
+        # создадим, чтобы было что удалять
+        new_doc = {
+            "type": 'INN',
+            "number": '123',
+            "name": 'Ivan Petrov'
+        }
+
+        new_doc_number = '123'
+        new_doc_shelf_number = '10'
+
+        documents.append(new_doc)
+        append_doc_to_shelf(new_doc_number, new_doc_shelf_number)
+
+        # проверяем, что оно создалось
+        assert is_created
+
+        # удаляем, что только что создали
+        delete()
+        # проверяем, что оно удалилось
+        assert is_deleted
+
+    # def test_delete_doc(self):  # D
+    #     user_doc_number = '123'
+    #     delete_doc()
+    #     self.assertEqual(remove_doc_from_shelf(user_doc_number), user_doc_number)
 
     def test_show_all_docs_info(self):  # L
         self.assertEqual(show_all_docs_info(), ('passport "2207 876234" "Василий Гупкин" NL', 'invoice "11-2" "Геннадий Покемонов" NL', 'insurance "10006" "Аристарх Павлов"'))
